750d55949f4f5b38fc423e15c00465d6c4dcab07
TechBuilderTeam <saashid18@gmail.com>
2024-11-04 13:08:32 +0600

    initial react app setup done

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import "./index.css";
+import Home from "./components/Home/Home";
+import NotFound from "./components/NotFound/NotFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
